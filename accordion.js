fe61f91d8d6404ff3e51a42f223ad695195c49e2
Paweł Bednarski <pawel.bednarski@meestergroup.pl>
2024-08-16 12:46:09 +0200

    init

--- accordion.js
+++ accordion.js
@@ -1,6 +1,6 @@
 (function () {
   function initAccordion() {
-    console.log("tak tak");
+    console.log("tak tak, nie nie");
   }
 
   document.addEventListener("DOMContentLoaded", initAccordion);
